badb8b803e43ea76769d1cd148cf7d8351fb4214
BlackBox <1647010108@qq.com>
2022-08-24 12:05:38 +0800

    feat(api):添加拦截器
    InterceptorConfig 配置拦截器
    TokenInterceptor 配置拦截规则
    添加错误代码
    
    经过测试
    [结束]

--- store/api/src/main/java/org/blackbox/store/api/interceptor/TokenInterceptor.java
+++ store/api/src/main/java/org/blackbox/store/api/interceptor/TokenInterceptor.java
@@ -19,7 +19,11 @@ public class TokenInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws Exception {
 
-        String token = request.getParameter("token");
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())){
+            return true;
+        }
+
+        String token = request.getHeader("token");
         if (token == null) {
             ResultVO resultVO = new ResultVO(ResStatus.NO, "请先登录", null);
             doResponse(response, resultVO);
