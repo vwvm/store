2b8ed1a7a11fd2c12e36d2fefbaa4a80d7f1a5f9
BlackBox <1647010108@qq.com>
2022-09-02 12:51:20 +0800

    feat(api,servers): 添加Async
    添加Async,且经过测试
    
    测试完成

--- store/services/src/main/java/org/blackbox/store/services/service/impl/AsyncServerImpl.java
+++ store/services/src/main/java/org/blackbox/store/services/service/impl/AsyncServerImpl.java
@@ -1,40 +1,20 @@
-package org.blackbox.store.api.config;
+package org.blackbox.store.services.service.impl;
 
-import org.blackbox.ApiApplication;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.stereotype.Component;
 
 import java.util.Random;
 import java.util.concurrent.Future;
 
-//
-@SpringBootTest(classes = ApiApplication.class)
-@ExtendWith(SpringExtension.class)
-public class Main {
-
-    @Test
-    void async() throws InterruptedException {
-        Long start = System.currentTimeMillis();
-        Future<String> one = doTaskOne();
-        Future<String> two = doTaskTwo();
-        Future<String> three = doTaskThree();
-
-        Long end = System.currentTimeMillis();
-
-        while (!(one.isDone() == two.isDone() == three.isDone())){
-            Thread.sleep(500);
-        }
-        System.out.printf("总耗时：%d \n", end - start);
-    }
+@Component
+public class AsyncServerImpl {
 
     Random random = new Random();
 
-    @Async("taskExecutor")
-    Future<String> doTaskOne() throws InterruptedException {
+    @Async()
+    public Future<String> doTaskOne() throws InterruptedException {
         System.out.println("开始任务一");
         Long start = System.currentTimeMillis();
         Thread.sleep(random.nextInt(10000));
@@ -42,8 +22,8 @@ public class Main {
         System.out.printf("完成任务一，耗时 %d\n" ,end - start);
         return new AsyncResult<>("第一任务完成");
     }
-    @Async("taskExecutor")
-    Future<String> doTaskTwo() throws InterruptedException {
+    @Async()
+    public Future<String> doTaskTwo() throws InterruptedException {
         System.out.println("开始任务二");
         Long start = System.currentTimeMillis();
         Thread.sleep(random.nextInt(10000));
@@ -51,8 +31,8 @@ public class Main {
         System.out.printf("完成任务二，耗时 %d \n" ,end - start);
         return new AsyncResult<>("第二任务完成");
     }
-    @Async("taskExecutor")
-    Future<String> doTaskThree() throws InterruptedException {
+    @Async()
+    public Future<String> doTaskThree() throws InterruptedException {
         System.out.println("开始任务三");
         Long start = System.currentTimeMillis();
         Thread.sleep(random.nextInt(10000));
