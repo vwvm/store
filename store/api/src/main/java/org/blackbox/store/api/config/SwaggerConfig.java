b25f4bd17eac66829ffa785310bb75b38d003c94
BlackBox <1647010108@qq.com>
2022-09-07 10:38:43 +0800

    feat(agent)：提交新功能
    添加结构
    
    添加完成

--- store/api/src/main/java/org/blackbox/store/api/config/SwaggerConfig.java
+++ store/api/src/main/java/org/blackbox/store/api/config/SwaggerConfig.java
@@ -17,29 +17,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableOpenApi
 public class SwaggerConfig {
 
-    // swagger2 会帮助我们生成接口文档
-    /*@Bean
-    public Docket getDocket(){
-
-        //生成封面信息
-        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-        apiInfoBuilder.title("商城后端接口说明")
-                .description("本文档详细说明了项目后端的接口规范")
-                .version("1.0.0")
-                .contact(new Contact("blackbox", "", "1647010108@qq.com"));
-        ApiInfo apiInfo = apiInfoBuilder.build();
-        //指定文档风格
-        Docket docket = new Docket(DocumentationType.SWAGGER_2);
-        //生成策略
-        docket.apiInfo(apiInfo)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("org.blackbox.store.api.controller"))
-                .paths(PathSelectors.any())
-                .build();
-
-        return docket;
-    }*/
-
     //swagger3
     @Bean
     public Docket docket(){
