cd90647f5902f01584e69910faa8de65be235a8d
BlackBox <1647010108@qq.com>
2023-02-24 22:36:59 +0800

    refactor(micro_service): 初始化微服务
    初始化了micro_service模块
    添加了子模块用于实现：
    springboot3下的spring cloud alibaba
    
    添加新的后台管理服务：
    areaManagementFunction
    
    添加成功

---
+++ micro_service/serviceManagement/src/main/java/org/vwvm/store/microservice/servicemanagement/ServiceManagementApplication.java
@@ -0,0 +1,13 @@
+package org.vwvm.store.microservice.servicemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceManagementApplication.class, args);
+    }
+
+}
---
+++ micro_service/serviceManagement/src/test/java/org/vwvm/store/microservice/servicemanagement/ServiceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vwvm.store.microservice.servicemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
