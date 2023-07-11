f3a969736e349f78ef1560568b60b29bfd03e5af
BlackBox <1647010108@qq.com>
2023-07-11 21:37:20 +0800

    feat(Translate): 添加翻译(Translate)
    添加网易云python的demo
    添加翻译
    
    添加完成

---
+++ python/example/main.py
@@ -0,0 +1,17 @@
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit
+import sys
+
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.mainLayout = QVBoxLayout()
+        self.setLayout(self.mainLayout)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    app.exec()
