8db8531b6e8fe924779bdc8b25ce51750f0abf59
BlackBox <1647010108@qq.com>
2024-05-07 09:39:14 +0800

    Optimize scum script files
    Added the ability to automatically pick up the backpack

--- python/script/game/scum/AutoExercise.py
+++ python/script/game/scum/AutoExercise.py
@@ -1,4 +1,60 @@
 import pyautogui
 
-pyautogui.sleep(3)
-pyautogui.keyDown('w')
+
+def forward_and_backward():
+    for _ in range(10):
+        pyautogui.keyDown('w')
+        pyautogui.sleep(6)
+        pyautogui.keyUp('w')
+        pyautogui.sleep(0.3)
+        pyautogui.keyDown('s')
+        pyautogui.sleep(5.5)
+        pyautogui.keyUp('s')
+        pyautogui.sleep(0.3)
+
+
+def start():
+    while True:
+        forward_and_backward()
+
+        pyautogui.keyDown('x')
+        pyautogui.sleep(0.3)
+        pyautogui.keyUp('x')
+        pyautogui.sleep(0.3)
+
+        pyautogui.keyDown('7')
+        pyautogui.sleep(0.3)
+        pyautogui.keyUp('7')
+        pyautogui.sleep(20)
+
+        forward_and_backward()
+
+        pyautogui.keyDown('x')
+        pyautogui.sleep(0.3)
+        pyautogui.keyUp('x')
+        pyautogui.sleep(0.3)
+
+        take_bag()
+
+
+def take_bag():
+    pyautogui.keyDown('tab')
+    pyautogui.sleep(0.1)
+    pyautogui.keyUp('tab')
+    pyautogui.sleep(0.1)
+    pyautogui.moveRel(-1000, -1000, duration=1)
+    pyautogui.moveRel(345, 195, duration=0.5)
+    pyautogui.click(button='right')
+    pyautogui.moveRel(0, 40, duration=0.5)
+    pyautogui.click(button='left')
+    pyautogui.keyDown('tab')
+    pyautogui.sleep(0.1)
+    pyautogui.keyUp('tab')
+    pyautogui.sleep(0.1)
+
+
+if __name__ == '__main__':
+    # start()
+    pyautogui.sleep(3)
+    start()
+    pass
